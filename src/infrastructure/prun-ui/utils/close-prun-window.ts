f5965293f94a6e148b55525f139f1a5dc477568e
Andrei Andreev <razenpok@gmail.com>
2026-06-22 21:58:52 +0200

    Use closePrunWindow in more places
    (cherry picked from commit 47f24ad0b30aa5075d8f0f3d7c96ab38d16bc369)

--- src/infrastructure/prun-ui/utils/close-prun-window.ts
+++ src/infrastructure/prun-ui/utils/close-prun-window.ts
@@ -1,7 +1,7 @@
 import { clickElement } from '@src/util';
 import { PrunI18N } from '@src/infrastructure/prun-ui/i18n';
 
-export async function closeWindow(window: HTMLElement | null | undefined) {
+export async function closePrunWindow(window: Element | null | undefined) {
   if (!window) {
     return;
   }
@@ -9,4 +9,10 @@ export async function closeWindow(window: HTMLElement | null | undefined) {
   const close = PrunI18N['Window.action.close']?.[0]?.value;
   const button = _$$(window, C.Window.button).find(x => x.textContent === close);
   await clickElement(button);
+}
+
+export async function closeTileWindow(tile: PrunTile) {
+  if (!tile.docked) {
+    await closePrunWindow(tile.window);
+  }
 }
