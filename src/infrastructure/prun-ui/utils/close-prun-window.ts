bfdeb33486337257c471762e092ac6ba182fff88
Andrei Andreev <razenpok@gmail.com>
2026-06-22 22:31:14 +0200

    Fix window resizing bug
    (cherry picked from commit d2f5bdfa87e9c6be658923fddfc1b7ef17e8e8e6)

--- src/infrastructure/prun-ui/utils/close-prun-window.ts
+++ src/infrastructure/prun-ui/utils/close-prun-window.ts
@@ -1,18 +1,19 @@
-import { clickElement } from '@src/util';
 import { PrunI18N } from '@src/infrastructure/prun-ui/i18n';
 
-export async function closePrunWindow(window: Element | null | undefined) {
+export function closePrunWindow(window: Element | null | undefined) {
   if (!window) {
     return;
   }
 
   const close = PrunI18N['Window.action.close']?.[0]?.value;
-  const button = _$$(window, C.Window.button).find(x => x.textContent === close);
-  await clickElement(button);
+  const button = _$$(window, C.Window.button).find(
+    x => x.textContent === close,
+  ) as HTMLButtonElement;
+  button.click();
 }
 
-export async function closeTileWindow(tile: PrunTile) {
+export function closeTileWindow(tile: PrunTile) {
   if (!tile.docked) {
-    await closePrunWindow(tile.window);
+    closePrunWindow(tile.window);
   }
 }
